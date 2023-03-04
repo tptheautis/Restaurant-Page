afdd3db1cc69141324079e684b0b08689ded7bf2
Theautis <116971196+tptheautis@users.noreply.github.com>
2023-03-04 14:03:57 -0800

    created button elements for navigation

--- src/navigation.js
+++ src/navigation.js
@@ -1,4 +1,14 @@
 const createNavigation = () => {
+  const content = document.getElementById('content');
+
+  const button1 = document.createElement('div');
+  const button2 = document.createElement('div');
+  const button3 = document.createElement('div');
+
+  content.append(button1);
+  content.append(button2);
+  content.append(button3);
+
   
 };
 
